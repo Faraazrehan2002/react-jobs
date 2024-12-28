916776faab1d5500678451f910d4bd35ebd756f6
Faraazrehan2002 <faraazrehan2002@gmail.com>
2024-12-29 01:36:11 +0530

    Update Job

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ import JobsPage from './pages/JobsPage'
 import NotFoundPage from './pages/NotFoundPage'
 import JobPage, {jobLoader} from './pages/JobPage'
 import AddJobPage from './pages/AddJobPage'
+import EditJobPage from './pages/EditJobPage'
 
 
 
@@ -34,6 +35,18 @@ const App = () => {
 
     return;
   }
+
+  // Update Job
+  const updateJob = async(job) => {
+    const res = await fetch(`/api/jobs/${job.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(job),
+    });
+    return;
+  };
   
   const router = createBrowserRouter(
         createRoutesFromElements(
@@ -42,6 +55,7 @@ const App = () => {
             <Route path = '/jobs' element = {<JobsPage/>}/>
             <Route path = '/jobs/:id' element = {<JobPage deleteJob = {deleteJob}/>} loader = {jobLoader}/>
             <Route path = '/add-job' element = {<AddJobPage addJobSubmit={addJob}/>}/>
+            <Route path = '/edit-job/:id' element = {<EditJobPage updateJobSubmit={updateJob}/>} loader = {jobLoader}/>
             <Route path = '*' element = {<NotFoundPage/>}/>
           </Route>)
   )
